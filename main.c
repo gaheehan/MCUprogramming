adb206c883779480271f1f3bccb7d5b06efdef85
Gahee <115511466+gaheehan@users.noreply.github.com>
2022-12-26 14:17:52 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
 /*
- * GccApplication6.c
+ * GccApplication7.c
  *
- * Created: 2022-12-19 오후 2:46:11
+ * Created: 2022-12-20 오전 9:24:30
  * Author : 21
  */ 
-
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <util/delay.h>
 
 
 int main(void)
-{	char pattern = 0b00000001;
+{
+	int i = 0;
+	char number[] = {0x7e, 0x60, 0x3d, 0x79, 0x63, 0x5b,0x5f, 0x72, 0x7f, 0x73};
+	DDRE = 0xff; //출력포트	
 	DDRD = 0x00; //입력포트
-	DDRB = 0xff; //출력포트
 	DDRF = 0xff; PORTF = 0xff; //신호발생(출력)
-	
-	PORTB = 0x01;
     while (1) 
     {
-		if(((PIND & 0b00000010) >> 1) == 0){
-			pattern = (pattern)<<1;
-			_delay_ms(500);
-		}
-		PORTB = pattern;
+		if(((PIND & 0b000010) >> 1) == 0){
+			i++;
+			if(i > 9)
+				i=0;
+			PORTE = ~number[i];
+			_delay_ms(1000);
+			}
+		
+		
+		if(((PIND & 0b000100) >> 2) == 0){
+			i--;
+			if(i < 0)
+				i = 9;
+			PORTE = ~number[i];
+			_delay_ms(1000);
+			}
 	}
-	
+	return 0;
 }
 
